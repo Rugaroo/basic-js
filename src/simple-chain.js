a6bdfbeef55a07752ed6d08a0b9b3e56bcf6bdd5
Rugaroo <63457972+Rugaroo@users.noreply.github.com>
2020-10-29 01:50:46 +0300

    feat: add solution make chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,36 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (value === undefined) {
+      this.chain.push('(  )');
+      return this
+    }else {
+      this.chain.push('( ' + value + ' )')
+    return this
+    }
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (position > this.chain.length || !Number.isInteger(position) || position < 1) {
+      this.chain.length = 0;
+      throw Error();
+    } else {
+      this.chain.splice(position - 1, 1);
+        return this;
+    }
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let answer = this.chain.join('~~');
+    this.chain.length = 0;
+    return answer;
   }
 };
 
